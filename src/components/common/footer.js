54cd68bd20b62dd5749c449a866d2c1c430240fe
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-12-07 19:35:37 +0300

    Dove img flip

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -10,7 +10,7 @@ export default class Header extends React.Component {
                 <img src={require('./dove.png') } alt='HOME' width="30" height="25"/>
           </Link>
           <Link to ="/home" className="img-foot">
-                <img src={require('./dove.png') } alt='HOME' width="30" height="25"/>
+                <img src={require('./dove-flip.png') } alt='HOME' width="30" height="25"/>
           </Link>
           <nav className="nav">  Quick Links >> <Link to="/">HOME</Link> | <Link to="/search">SEARCH</Link>   |   <Link to="/categories">CATEGORIES</Link> | <Link to="/add/ ">ADD</Link> </nav>
 
