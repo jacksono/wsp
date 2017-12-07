ef557faac742df521fa2ab30e70982c240d8e0f8
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-12-07 14:20:17 +0300

    Add footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, {PropTypes} from 'react';
 import Header from './common/header'
+import Footer from './common/footer'
 
 class App extends React.Component {
     render() {
         return (
           <div className="body">
           <span className="glyphicon "/>
-              < Header />
+              <Header />
             {this.props.children}
+            <Footer />
           </div>
         );
     }
