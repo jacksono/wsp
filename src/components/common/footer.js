5dbd6ac100441ae45531c8a15c3bb7973180c617
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-12-07 18:44:42 +0300

    Add quick links

--- src/components/common/footer.js
+++ src/components/common/footer.js
@@ -12,7 +12,7 @@ export default class Header extends React.Component {
           <Link to ="/home" className="img-foot">
                 <img src={require('./dove.png') } alt='HOME' width="30" height="25"/>
           </Link>
-          <nav className="nav">  <Link to="/">HOME</Link> | <Link to="/search">SEARCH</Link>   |   <Link to="/categories">CATEGORIES</Link> | <Link to="/add/ ">ADD</Link> </nav>
+          <nav className="nav">  Quick Links >> <Link to="/">HOME</Link> | <Link to="/search">SEARCH</Link>   |   <Link to="/categories">CATEGORIES</Link> | <Link to="/add/ ">ADD</Link> </nav>
 
         </div>
     );
