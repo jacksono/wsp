bfd4dc50e210220bc57ca11aef4429d2a4d021f1
ONYANGO JACKSON JOHN <jackson.onyango@andela.com>
2017-12-13 18:42:01 +0300

    Change db

--- src/components/apiHelper.js
+++ src/components/apiHelper.js
@@ -2,8 +2,8 @@ import * as request from 'superagent';
 
 export default function apiCall(data, type, url) {
   const contextPath = location.href.split('/')[3];
-  const BASE_URL = `http://localhost:5000/api/v1/${url}`;
-  // const BASE_URL = `https://wsp-api.herokuapp.com/api/v1/${url}`;
+  // const BASE_URL = `http://localhost:5000/api/v1/${url}`;
+  const BASE_URL = `https://wsp-api.herokuapp.com/api/v1/${url}`;
   return new Promise((resolve, reject) => {
     request[type](BASE_URL)
       .send(data)
